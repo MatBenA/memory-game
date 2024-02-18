5440ea733c953f8b73a49980581aa6d9589c384d
Matías <crosserain@gmail.com>
2024-02-18 17:11:52 -0300

    states added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [score, setScore] = useState(0);
+    const handleScore = () => setScore((prev) => prev + 1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [bestScore, setBestScore] = useState(0);
+
+    return (
+        <>
+            <h1>Memory Card Game!</h1>
+            <p>Score: {score}</p>
+            <p>Best Score: {bestScore}</p>
+            <p>
+                Get points by clicking on an image but do not click on any more
+                than once!
+            </p>
+            <section>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+                <div className="card" onClick={handleScore}>tarjeta</div>
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
